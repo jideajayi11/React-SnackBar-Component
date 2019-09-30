91f911a42e0bf915515764c7e9a76791313757de
jideajayi11 <jideajayi11@gmail.com>
2019-09-30 10:54:47 +0100

    publish snackbar component

--- src/index.js
+++
@@ -1,6 +0,0 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-import SnackBar from './SnackBar.Component';
-
-// ReactDOM.render(<SnackBar isShowing={true} text='i love me i love me i love me' displayTime={9000} customClass="vmeme" showCloseBtn={true} />, document.getElementById('root'));
-export default SnackBar;
